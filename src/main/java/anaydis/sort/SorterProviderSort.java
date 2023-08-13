e23b842652e7193730a935cf1fcc2fc0925a1942
Santos <santosbogo@gmail.com>
2023-08-13 19:09:13 -0300

    Sorter provider implemented with HashMap

--- src/main/java/anaydis/sort/SorterProviderSort.java
+++ src/main/java/anaydis/sort/SorterProviderSort.java
@@ -2,14 +2,26 @@ package anaydis.sort;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 public class SorterProviderSort implements SorterProvider {
+
+    private Map<SorterType, Sorter> sorters = new EnumMap<>(SorterType.class);
+
+    public SorterProviderSort(){
+        sorters.put(SorterType.SELECTION, new SelectionSorter());
+        sorters.put(SorterType.INSERTION, new InsertionSorter());
+        sorters.put(SorterType.BUBBLE, new BubbleSorter());
+    }
+
     @Override
     public @NotNull Iterable<Sorter> getAllSorters() {
-        return null;//Hacerlo con hash map
+        return sorters.values();
     }
 
     @Override
     public @NotNull Sorter getSorterForType(@NotNull SorterType type) {
-        return null;
+        return sorters.get(type);
     }
 }
