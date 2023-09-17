16bede11df5f9f1c2ceed00cb9e796de12a2266a
Santos <santosbogo@gmail.com>
2023-09-17 20:48:16 -0300

    Renewed TST and some practice for the exam

--- src/main/java/anaydis/search/TSTTrieMap.java
+++ src/main/java/anaydis/search/TSTTrieMap.java
@@ -69,18 +69,19 @@ public class TSTTrieMap<T> implements Map<String, T> {
 
     private Tuple<T> put(Node<T> node, String key, T value, int level) {
         char c = key.charAt(level);
-        T oldValue = null;
+        T oldValue = value;
 
         if (node == null) {
             node = new Node<>();
             node.c = c;
+        }else{
+            oldValue = node.value;
         }
 
         if (c < node.c) node.left = put(node.left, key, value, level).getNode();
         else if (c > node.c) node.right = put(node.right, key, value, level).getNode();
         else if (level < key.length() - 1) node.middle = put(node.middle, key, value, level + 1).getNode();
         else {
-            oldValue = node.value;
             if (oldValue == null) {
                 size++;
             }
