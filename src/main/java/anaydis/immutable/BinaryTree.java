732d2005ddbde382e86009ff9e57d56d09ffb3de
Santos <santosbogo@gmail.com>
2023-10-06 11:30:04 -0300

    Problems with diamond operator not supported in teamcity

--- src/main/java/anaydis/immutable/BinaryTree.java
+++ src/main/java/anaydis/immutable/BinaryTree.java
@@ -7,53 +7,51 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Queue;
 
-public class BinaryTree<K, V> implements Map<K, V>{
-    private int size = 0;
-    private Node root;
-    Comparator<K> comparator;
+public class BinaryTree<K, V> implements Map<K, V> {
+    private final Node root;
+    private final Comparator<K> comparator;
 
-    public BinaryTree(Comparator<K> comparator){
+    public BinaryTree(Comparator<K> comparator) {
         this.comparator = comparator;
+        this.root = null;
     }
 
-    private BinaryTree(Comparator<K> comparator, Node root, int size){
+    private BinaryTree(Comparator<K> comparator, Node root) {
         this.comparator = comparator;
         this.root = root;
-        this.size = size;
     }
 
-    private class Node{
+    private class Node {
         final K key;
         final V value;
         final Node left;
         final Node right;
+        final int size;
 
-        Node(K key, V value, Node left, Node right){
+        Node(K key, V value, Node left, Node right, int size) {
             this.key = key;
             this.value = value;
             this.left = left;
             this.right = right;
+            this.size = size;
         }
 
-        Node(K key, V value){
-            this.key = key;
-            this.value = value;
-            this.left = null;
-            this.right = null;
+        Node(K key, V value) {
+            this(key, value, null, null, 1);
         }
     }
 
     @Override
     public int size() {
-        return size;
+        return root == null ? 0 : root.size;
     }
 
-    private Node find(Node node, K key){
+    private Node find(Node node, K key) {
         if (node == null) return null;
 
         int comp = comparator.compare(key, node.key);
 
-        if(comp < 0) return find(node.left, key);
+        if (comp < 0) return find(node.left, key);
         else if (comp > 0) return find(node.right, key);
         else return node;
     }
@@ -72,32 +70,37 @@ public class BinaryTree<K, V> implements Map<K, V>{
     @Override
     public Map<K, V> put(@NotNull K key, V value) {
         Node newRoot = put(root, key, value);
-        if (newRoot == root) {
-            return this; // No changes were made
-        }
-        return new BinaryTree<>(comparator, newRoot, size + 1);
+        return new BinaryTree<>(comparator, newRoot);
     }
-    private Node put(Node node, K key, V value){
+
+    private Node put(Node node, K key, V value) {
         if (node == null)
             return new Node(key, value);
 
         int cmp = comparator.compare(key, node.key);
+        int newSize = node.size;
 
-        if (cmp < 0)
-            return new Node(node.key, node.value, put(node.left, key, value), node.right);
-        else if (cmp > 0)
-            return new Node(node.key, node.value, node.left, put(node.right, key, value));
-        else
-            return new Node(key, value, node.left, node.right);
+        if (cmp < 0) {
+            Node newLeft = put(node.left, key, value);
+            newSize = 1 + newLeft.size + (node.right == null ? 0 : node.right.size);
+            return new Node(node.key, node.value, newLeft, node.right, newSize);
+        } else if (cmp > 0) {
+            Node newRight = put(node.right, key, value);
+            newSize = 1 + (node.left == null ? 0 : node.left.size) + newRight.size;
+            return new Node(node.key, node.value, node.left, newRight, newSize);
+        } else {
+            return new Node(key, value, node.left, node.right, node.size);
+        }
     }
 
     @Override
     public Iterator<K> keys() {
-        Queue<K> keys = new ArrayDeque<K>();
+        Queue<K> keys = new ArrayDeque<>();
         inOrder(root, keys);
         return keys.iterator();
     }
-    private void inOrder(Node node, Queue<K> keys){
+
+    private void inOrder(Node node, Queue<K> keys) {
         if (node == null) return;
         inOrder(node.left, keys);
         keys.add(node.key);
---
+++ src/main/java/anaydis/search/HeapPriorityQueue.java
@@ -0,0 +1,4 @@
+package anaydis.search;
+
+public class HeapPriorityQueue {
+}
