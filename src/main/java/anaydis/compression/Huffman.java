04360a1a89cfde794ca54dfd425a304b3abf92cd
Santos <santosbogo@gmail.com>
2023-10-22 16:57:49 -0300

    Huffmannn changes

--- src/main/java/anaydis/compression/Huffman.java
+++ src/main/java/anaydis/compression/Huffman.java
@@ -12,7 +12,10 @@ import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.util.*;
 
-public class Huffman implements Compressor{
+public class Huffman implements Compressor {
+
+    private static final int EOF = -1;  // End-of-file marker
+
     @Override
     public void encode(@NotNull InputStream input, @NotNull OutputStream output) throws IOException {
         List<Character> chars = inputToString(input);
@@ -20,14 +23,15 @@ public class Huffman implements Compressor{
         PriorityQueue<Node> table = generateHuffmanTree(concurrenceMap);
         Map<Integer, Bits> symbolTable = createSymbolTable(table);
 
-        //Write the size of the symbol table
+        // Write the size of the symbol table (using 2 bytes)
+        output.write(symbolTable.size() >> 8);
         output.write(symbolTable.size());
 
-        //Write the symbol table
-        for (final Map.Entry<Integer, Bits> current : symbolTable.entrySet()) {
-            final Integer character = current.getKey();
+        // Write the symbol table
+        for (Map.Entry<Integer, Bits> current : symbolTable.entrySet()) {
+            Integer character = current.getKey();
             output.write(character);
-            final Bits bits = current.getValue();
+            Bits bits = current.getValue();
             bits.writeInto(output);
         }
 
@@ -38,70 +42,52 @@ public class Huffman implements Compressor{
         }
 
         // Write the message
-        final BitsOutputStream contenido = new BitsOutputStream();
-        for (int character : chars) {
-            final Bits code = symbolTable.get(character);
+        BitsOutputStream contenido = new BitsOutputStream();
+        for (char character : chars) {
+            Bits code = symbolTable.get((int) character);
             contenido.write(code);
         }
         output.write(contenido.toByteArray());
-
     }
 
     private List<Character> inputToString(InputStream input) throws IOException {
         List<Character> chars = new ArrayList<>();
-        char current = (char) input.read();
-
-        while (current != -1){
-            chars.add(current);
-            current = (char) input.read();
+        int readChar;
+        while ((readChar = input.read()) != EOF) {
+            chars.add((char) readChar);
         }
-
         return chars;
     }
 
-    private HashMap<Character, Integer> concurrenceMap(List<Character> chars){
+    private HashMap<Character, Integer> concurrenceMap(List<Character> chars) {
         HashMap<Character, Integer> concurrenceMap = new HashMap<>();
-
-        for (char c: chars)
-            if (concurrenceMap.containsKey(c))
-                concurrenceMap.put(c, concurrenceMap.get(c) + 1);
-            else
-                concurrenceMap.put(c, 1);
-
+        for (char c : chars) {
+            concurrenceMap.put(c, concurrenceMap.getOrDefault(c, 0) + 1);
+        }
         return concurrenceMap;
     }
 
-    private PriorityQueue<Node> generateHuffmanTree(HashMap<Character, Integer> concurrenceMap){
+    private PriorityQueue<Node> generateHuffmanTree(HashMap<Character, Integer> concurrenceMap) {
         PriorityQueue<Node> table = new OrderedArrayPriorityQueue<>(Node::compareTo);
-
-        for (char i : concurrenceMap.keySet()) {
-            Node node = new Node(i, concurrenceMap.get(i));
+        for (Map.Entry<Character, Integer> entry : concurrenceMap.entrySet()) {
+            Node node = new Node(entry.getKey(), entry.getValue());
             table.insert(node);
         }
-
-        while (table.size() != 1) {
+        while (table.size() > 1) {
             Node left = table.pop();
             Node right = table.pop();
             Node node = new Node(left, right, left.value + right.value);
             table.insert(node);
         }
-
         return table;
     }
 
-
     private Map<Integer, Bits> createSymbolTable(PriorityQueue<Node> table) {
-        //No symbols
         if (table.isEmpty()) return Collections.emptyMap();
-
-        //Just one symbol
-        if (table.peek().isLeaf()) return Collections.singletonMap(table.pop().value, new Bits().add(false));
-
-        //Many symbols
-        final Node root = table.pop();
-        final Map<Integer, Bits> result = new LinkedHashMap<>();
+        if (table.peek().isLeaf()) return Collections.singletonMap((int) table.pop().character, new Bits().add(false));
+        Node root = table.pop();
+        Map<Integer, Bits> result = new LinkedHashMap<>();
         root.collect(result, new Bits());
-
         return result;
     }
 
@@ -119,7 +105,7 @@ public class Huffman implements Compressor{
         for (int i = 0; i < total; i++) {
             Integer character = null;
             while (character == null) {
-                currentBits.add(bitAt(input.read(), 7)); // Asumiendo que lees un bit a la vez.
+                currentBits.add(bitAt(input.read(), 7));  // Read bit-by-bit
                 character = symbolTable.get(currentBits);
             }
             output.write(character);
@@ -127,15 +113,13 @@ public class Huffman implements Compressor{
     }
 
     private Map<Bits, Integer> readSymbolTable(InputStream input) throws IOException {
-        int symbols = input.read();
+        int symbols = (input.read() << 8) + input.read();
         Map<Bits, Integer> symbolTable = new HashMap<>();
-
         for (int i = 0; i < symbols; i++) {
             int character = input.read();
             Bits bits = readBits(input);
             symbolTable.put(bits, character);
         }
-
         return symbolTable;
     }
 
@@ -144,28 +128,15 @@ public class Huffman implements Compressor{
         byte[] bytes = new byte[(length + 7) / 8];
         input.read(bytes);
         Bits bits = new Bits();
-
         for (int i = 0; i < length; i++) {
-            if (bitAt(bytes[i / 8], i % 8)) {
-                bits.add(true);
-            } else {
-                bits.add(false);
-            }
+            if (bitAt(bytes[i / 8], i % 8)) bits.add(true);
+            else bits.add(false);
         }
-
         return bits;
     }
 
     public static boolean bitAt(int value, int position) {
-        // Create a mask with a 1 at the specific position
-        int mask = 1 << (7 - position); // Assuming 8 bits in a byte (0 to 7)
-        // Use bitwise AND to check if the bit at the specified position is 1 or 0
+        int mask = 1 << (7 - position);
         return (value & mask) != 0;
     }
-    private boolean bitAt(byte[] bytes, int nth) {
-        final int pos = nth / 8;
-        return pos < bytes.length && (bytes[pos] >> ((8 * (pos + 1) - (nth + 1)) % 8) & 1 ) != 0;
-    }
-
 }
-
--- src/main/java/anaydis/compression/Node.java
+++ src/main/java/anaydis/compression/Node.java
@@ -1,16 +1,15 @@
 package anaydis.compression;
 
 import anaydis.bit.Bits;
-
 import java.util.Map;
 
 public class Node implements Comparable<Node> {
-    public final char character;  // El carácter representado por el nodo (solo relevante para las hojas)
-    public final int value;      // Frecuencia del carácter o suma de las frecuencias de los hijos
-    public final Node left;      // Hijo izquierdo
-    public final Node right;     // Hijo derecho
+    public final char character;
+    public final int value;
+    public final Node left;
+    public final Node right;
 
-    // Constructor para nodos hoja
+    // Constructor for leaf nodes
     public Node(char character, int value) {
         this.character = character;
         this.value = value;
@@ -18,9 +17,9 @@ public class Node implements Comparable<Node> {
         this.right = null;
     }
 
-    // Constructor para nodos internos
+    // Constructor for internal nodes
     public Node(Node left, Node right, int value) {
-        this.character = '\0';
+        this.character = '\0';  // Null character for non-leaf nodes
         this.value = value;
         this.left = left;
         this.right = right;
@@ -39,16 +38,13 @@ public class Node implements Comparable<Node> {
         if (isLeaf()) {
             table.put((int) character, prefix);
         } else {
-            if (left != null) {
-                Bits leftPrefix = prefix.copy();
-                leftPrefix.add(false);
-                left.collect(table, leftPrefix);
-            }
-            if (right != null) {
-                Bits rightPrefix = prefix.copy();
-                rightPrefix.add(true);
-                right.collect(table, rightPrefix);
-            }
+            Bits leftPrefix = prefix.copy();
+            leftPrefix.add(false);
+            left.collect(table, leftPrefix);
+
+            Bits rightPrefix = prefix.copy();
+            rightPrefix.add(true);
+            right.collect(table, rightPrefix);
         }
     }
 }
