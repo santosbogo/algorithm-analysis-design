1da04aef2d3fa786c58b5272afc4d8c7600a19ac
Santos <santosbogo@gmail.com>
2023-10-27 19:16:25 -0300

    Had a problem in the decode

--- src/main/java/anaydis/sandbox/compression/BurrowsWheeler.java
+++ src/main/java/anaydis/sandbox/compression/BurrowsWheeler.java
@@ -1,5 +1,6 @@
-package anaydis.compression;
+package anaydis.sandbox.compression;
 
+import anaydis.compression.Compressor;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
@@ -105,9 +106,54 @@ public class BurrowsWheeler implements Compressor {
         output.write(decodedMessage.toString().getBytes());
     }
 
+
+
     private Integer readOriginalMessagePosition(InputStream input) throws IOException {
         byte[] sizeBytes = new byte[4];
         input.read(sizeBytes);
         return ByteBuffer.wrap(sizeBytes).getInt();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        // Convert binary strings to bytes
+        String[] binaryStrings = {
+                "1100001", "1100001", "1100001", "1100001", "1100001", "1100001", "1100001", "1100001", "1100001",
+                "100000", "1100001", "1101101", "1100001", "1110011", "1100001", "100000", "1100001", "1101101",
+                "1100001", "1110011", "1100001", "100000", "1100001", "1101101", "1100001", "1110011", "1100001"
+        };
+
+        byte[] inputData = new byte[binaryStrings.length];
+        for (int i = 0; i < binaryStrings.length; i++) {
+            inputData[i] = (byte) Integer.parseInt(binaryStrings[i], 2);
+        }
+
+        ByteArrayInputStream input = new ByteArrayInputStream(inputData);
+
+        // Step 2: Encode the input
+        anaydis.compression.BurrowsWheeler bwt = new anaydis.compression.BurrowsWheeler();
+        ByteArrayOutputStream encodedOutput = new ByteArrayOutputStream();
+        bwt.encode(input, encodedOutput);
+
+        byte[] encodedBytes = encodedOutput.toByteArray();
+        System.out.println("Encoded Bytes:");
+        for (byte b : encodedBytes) {
+            System.out.print(b + " ");
+        }
+        System.out.println();
+
+        // Step 3: Decode the encoded message
+        ByteArrayInputStream encodedInput = new ByteArrayInputStream(encodedBytes);
+        ByteArrayOutputStream decodedOutput = new ByteArrayOutputStream();
+        bwt.decode(encodedInput, decodedOutput);
+
+        byte[] decodedBytes = decodedOutput.toByteArray();
+        System.out.println("Decoded Bytes:");
+        for (byte b : decodedBytes) {
+            System.out.print(b + " ");
+        }
+        System.out.println();
+
+
     }
 }
