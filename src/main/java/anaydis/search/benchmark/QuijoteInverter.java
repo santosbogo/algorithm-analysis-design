72cdb0fc9ef8c0462049c62ca8ecdedea64b48e7
Santos <santosbogo@gmail.com>
2025-03-14 22:44:23 -0300

    refactor

--- src/main/java/anaydis/search/benchmark/QuijoteInverter.java
+++ src/main/java/anaydis/search/benchmark/QuijoteInverter.java
@@ -1,4 +1,4 @@
-package anaydis.search;
+package anaydis.search.benchmark;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -8,9 +8,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class QuijoteInverter {
     public static void main(String[] args) {
